57feb891abdf76eeaaf5c5e71369fd34a7357a35
aleenaamir13 <aleena13f@gmail.com>
2026-06-20 14:40:31 +0500

    changes and book formation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,14 +3,16 @@ import React from 'react'
 import Link from "next/link";
 import Image from "next/image";
 import {usePathname} from "next/navigation";
-import {cn} from "@/lib/utils"
+import {cn} from "@/lib/utils";
+import { Show, SignInButton, SignUpButton, UserButton,useUser } from '@clerk/nextjs';
 
 const navItems=[
     {label:"Library",href:"/"},
     {label:"Add New",href:"/books/new"},
 ]
 const Navbar = () => {
-    const PathName=usePathname()
+    const PathName=usePathname();
+    const {user}=useUser();
     return (
         <header className="w-full fixed z-50 bg-(--bg-primary)">
             <div className="wrapper navbar-height py-4 flex justify-between items-center">
@@ -27,8 +29,27 @@ const Navbar = () => {
                         )
                     }
                     )}
+                    <div className="flex gap-7.5 items-center">
+                    <Show when="signed-out">
+                        <SignInButton />
+                        <SignUpButton>
+                            <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                                Sign Up
+                            </button>
+                        </SignUpButton>
+                    </Show>
+                    <Show when="signed-in">
+                        <div className="nav-user-link">
+                        <UserButton />
+                        </div>
+                        {user?.firstName && (
+                            <Link href="/subscriptions" className="nav-user-name">
+                                {user.firstName}
+                            </Link>
+                        )}
+                    </Show>
+                        </div>
                 </nav>
-
             </div>
         </header>
     )
